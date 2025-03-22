d136d334b740b57dcb091a8cb36103aaf521ea62
Jamesunekwuojo <unekwuojoabah4@gmail.com>
2025-03-22 03:39:39 +0100

    removed backend folder

--- backend/routes/authcheckRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { checkAuth } from '../controllers/authcheckController.js';
-
-const router = express.Router();
-
-router.get('/', checkAuth);
-
-export default router;
--- backend/routes/redisRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { testRedis } from '../controllers/redisController.js';
-
-const router = express.Router();
-
-router.get('/', testRedis); 
-
-
-export default router;
